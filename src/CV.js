6eaa06b5ed595cea2c0c2f44f2eba2eb33d82e89
david <05779@farnborough.ac.uk>
2023-10-13 18:30:21 +0100

    Updated Demos and attched CV pdf

--- src/CV.js
+++ src/CV.js
@@ -1,14 +1,15 @@
 import React from "react";
+import ReactPDF from '@react-pdf/renderer';
 import { Document, Page } from "react-pdf";
 
-import resume from './assets/CV.pdf'
+import resume from './assets/cv.pdf'
 
 const CV = () => {
     return(
         <>
             <div className="container-fluid" id="cv-container">
                 <div className="row">
-                    <h1>Get the pdf to load in here already</h1>
+                    <object data={resume} type="application/pdf" width="100%" height="720px" />
                 </div>
                 <div className="row jutify-content-center text-center p-3">
                     <p className="h6">Having issues viewing this? <a id="link" href={resume} target="_blank">View in browser</a></p>
