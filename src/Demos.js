0640d2fe5d29768ba0ab1a76bcb967ddce9985ed
david <05779@farnborough.ac.uk>
2023-10-15 17:40:11 +0100

    export demos to an external array, moved external arrays into their own folder and corrected imports

--- src/Demos.js
+++ src/Demos.js
@@ -2,21 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import solitaire from './assets/solitaire snapshot.png';
 
+import examples from './data/demos'
+
 const Demos = () => {
     return(
         <>
             <div className="container-fluid text-light" style={{backgroundColor: "var(--jet)"}}>
                 <p className="h2 text-center p-4" style={{color: "var(--tiffany-blue)"}}>Here is just some of my work:</p>
-                <div className="row p-3">
-                    <div className="col justify-content-center">
-                        <img src={solitaire} className="rounded mx-auto d-block" style={{width: "80%"}}></img> {/* Make into a button*/ }
+                {
+                examples.map((example) => (
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-auto">
+                                <p id="example-title" className="display-3 text-start fw-bold" style={{color: "var(--tang-blue)"}}>{example.title}</p>
+                            </div>
+                        </div>
+                        <div className="row p-3">
+                            <div className="col justify-content-center">
+                                <img src={example.image} className="rounded mx-auto d-block" style={{width: "100%", border: "3px solid var(--tang-blue)"}}></img>
+                            </div>
+                            <div className="col-sm">
+                                <p className="h5 text-center">{example.text}</p><br />
+                                <p className="h5 text-center">{example.link}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-sm">
-                        <p className="h5 text-center">Recently I have been working on a Solitaire game. Using react to build the application and bootstrap for styling, I have created card elements who take all their data from a local storage then generate the game board.</p>
-                        <br /><p className="h5 text-center">Early on in the process I realised one of the key issues with solitaire is board generation. Randomly generating boards for each game is time consuming and so I should instead have a finite number of pre-made boards that I can randomly choose from. For now these are stored locally but in the future I will add a back-end server to hold a much larger number of these boards.</p>
-                        <br /><p className="h5 text-center">The project in it's current state is still a work on progress, to see teh code so far, check out the project's <Link id="link" to={"https://github.com/dfrommont/Solitaire"}>GitHub repository</Link>.</p>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
         </>
     )
---
+++ src/data/demos.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import solitaire from './../assets/solitaire snapshot.png';
+import structures from './../assets/Data Structures snapshot.png'
+
+const demos = [
+    {
+        "text": "Recently I have been working on a Solitaire game. Using react to build the application and bootstrap for styling, I have created card elements who take all their data from a local storage then generate the game board. Early on in the process I realised one of the key issues with solitaire is board generation. Randomly generating boards for each game is time consuming and so I should instead have a finite number of pre-made boards that I can randomly choose from. For now these are stored locally but in the future I will add a back-end server to hold a much larger number of these boards.\n The project in it's current state is still a work on progress, to see teh code so far, check out the project's GitHub repository:",
+        "image": solitaire,
+        "title": "Solitaire",
+        "link": <Link id="link" to={"https://github.com/dfrommont/Solitaire"}>GitHub repository</Link>
+    },
+    {
+        "text": "Coded alongside a first year module (Data Structures and Algorithms) I built up a repo of Java ADTs. Each type contains classes representing nodes that hold data and the structure that links all the nodes together as well as a main class and a test class. Documentation has also been egenrated for every type. The included ADTs are: Singly Linked list, Doubly Linked list, Queue ysing array, Queue using Doubly Linked list, Priority Queue, Binary Search tree, Stack using array and Stack using Doubly Linked list.",
+        "image": structures,
+        "title": "Java Data Structures",
+        "link": <Link id="link" to={"https://github.com/dfrommont/Java_Data_Structures"}>GitHub repository</Link>
+    }
+]
+
+export default demos;
